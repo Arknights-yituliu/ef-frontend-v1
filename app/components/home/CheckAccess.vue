d007f9f98ec73f51cef0553adf33b11c12ee4676
BioHazard <3482991796@qq.com>
2025-12-12 02:05:51 +0800

    fix(i18n): 改一下资格查询的翻译

--- app/components/home/CheckAccess.vue
+++ app/components/home/CheckAccess.vue
@@ -1,24 +1,25 @@
-<script lang="ts" setup>
-
-</script>
+<script lang="ts" setup></script>
 
 <template>
   <v-card class="home-navigation-card">
     <v-card-title>
       <div class="home-navigation-card-title">
-        <img alt="" class="home-navigation-card-title-icon" src="~/assets/icon/endfield-icon.png"/>
-        <b>{{ $t('component.home.checkAccess.comprehensiveTestQualificationCheck') }}</b>
-        <div class="flex-1"/>
+        <img alt="" class="home-navigation-card-title-icon" src="~/assets/icon/endfield-icon.png" />
+        <b>{{ $t('component.home.checkAccess.betaTestIICheckAccess') }}</b>
+        <div class="flex-1" />
       </div>
     </v-card-title>
     <v-card-text>
-      <img alt="" class="test-kv" src="https://cos.yituliu.cn/endfield/other/test-kv.jpg">
-      <v-card class="check-access-btn" hover
-              @click="()=>openNewPage('https://endfield.hypergryph.com/checkAccess')">
+      <img alt="" class="test-kv" src="https://cos.yituliu.cn/endfield/other/test-kv.jpg" />
+      <v-card
+        class="check-access-btn"
+        hover
+        @click="() => openNewPage('https://endfield.hypergryph.com/checkAccess')"
+      >
         <v-card-text>
           <div>
             <span>
-             {{ $t('component.home.checkAccess.checkAccessOnEndfieldOfficialSite') }}
+              {{ $t('component.home.checkAccess.checkAccessOnEndfieldOfficialSite') }}
             </span>
             <v-icon icon="mdi-open-in-new"></v-icon>
           </div>
@@ -26,11 +27,9 @@
       </v-card>
     </v-card-text>
   </v-card>
-
 </template>
 
 <style scoped>
-
 .check-access-btn {
   text-align: center;
   margin: var(--spacing-md) auto;
@@ -43,6 +42,4 @@
   display: block;
   margin: auto;
 }
-
-
 </style>
