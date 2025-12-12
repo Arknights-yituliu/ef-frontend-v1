c315f66b21fd5493e806e5e5ef241eedb745de26
BioHazard <3482991796@qq.com>
2025-12-13 01:40:13 +0800

    feat(攒抽计算器): 添加攒抽计算器页面

---
+++ app/pages/tools/gacha-calculator.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>{{ t('page.tools.gachaCalculator.title') }}</h1>
+  <p>{{ t('page.tools.gachaCalculator.underDevelopment') }}</p>
+</template>
+<script setup lang="ts">
+const { t } = useI18n();
+</script>
+
+<style scoped></style>
