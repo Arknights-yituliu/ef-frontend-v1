8fa3a7c698f04be4beac5a2af5440ee0953aa4bf
yamazakura <1969314158@qq.com>
2025-12-25 15:43:40 +0800

    feat:加一个开发用的物品名称对照表格

---
+++ app/pages/dev/i18n-table.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import { ref,onMounted, computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { itemTable, getLocalizedText } from '@/shared/utils/gameData/gameData';
+import { getItemIconUrl } from '@/shared/utils/gameData/item';
+import type { ItemTable } from '@/shared/types/endfielddata/TableCfg/ItemTable';
+
+
+let itemTableCollect = ref<ItemTableCollect[]>([])
+
+interface ItemTableCollect {
+  id: string
+  name: string
+}
+
+// 自定义表头配置
+const itemTableHeaders = [
+  { title: '物品图标' },
+  { title: '物品ID', key: 'id' },
+  { title: '物品名称', key: 'name' }
+]
+
+onMounted(() => {
+for(let itemId in itemTable.value){
+  if(!itemTable.value[itemId]) continue;
+  const item:Item = itemTable.value[itemId]
+  itemTableCollect.value.push({
+    id: itemId,
+    name: getLocalizedText(item.name || '', 'zh-CN'),
+  })
+}
+})
+
+const itemSearch = ref('')
+
+// 搜索过滤后的物品列表
+const filteredItems = computed(() => {
+  if (!itemSearch.value) {
+    return itemTableCollect.value
+  }
+  const searchText = itemSearch.value.toLowerCase()
+  return itemTableCollect.value.filter(item => 
+    item.name.toLowerCase().includes(searchText)
+  )
+})
+
+</script>
+
+<template>
+     
+  <v-data-table :items="filteredItems" :headers="itemTableHeaders" >
+      <template v-slot:top>
+      <v-text-field
+        v-model="itemSearch"
+        label="Search (根据物品中文名称搜索)"
+      ></v-text-field>
+    </template>
+     <template v-slot:item="{ item }">
+      <tr class="text-no-wrap">
+        <td> <img :src="getItemIconUrl(item.id)" :alt="item.name" style=""></td>
+        <td>{{ item.id }}</td>
+        <td>{{ item.name}}</td>
+      </tr>
+    </template>
+  </v-data-table>
+
+</template>
+
+<style scoped>
+  .i18n-item-table-icon{
+width: 50px; height: 50px;
+  }
+h1 {
+  color: red;
+}
+</style>
