4efe4db3ea0f7b1f2d8151922aa51f671334aee0
BioHazard <3482991796@qq.com>
2026-02-13 02:16:33 +0800

    chore: 更改 eslint 和 prettier 配置，使得 eslint 和 prettier 不再打架

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,16 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs';
+import prettier from 'eslint-config-prettier';
+import vuetify from 'eslint-config-vuetify';
 
-export default withNuxt({
-  files: ['**/*.ts', '**/*.vue'],
+export default withNuxt(vuetify, prettier, {
   rules: {
-    'no-console': 'off', // 允许 console.log 语句
-    '@typescript-eslint/no-unused-vars': 'off', // 允许未使用的变量和方法
-    '@typescript-eslint/no-explicit-any': 'off', // 允许 any 类型
+    '@typescript-eslint/no-explicit-any': 'off', // 允许使用 any 类型
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ], // 允许未使用的变量（以 _ 开头的参数或变量）
+    'vue/multi-word-component-names': 'off', // 允许单词组件名称
     'vue/valid-v-slot': 'off', // 允许 v-slot 指令使用修饰符
   },
 });
